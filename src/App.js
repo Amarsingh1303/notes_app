f1fc953d608bb04f698aef06acf5ce1068627b18
killingspray <amarsingh1303@gmail.com>
2020-08-23 23:16:04 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 // import Notes from "./Components/Notes";
-// import Header from "./Components/Header";
+import Header from "./Components/Header";
 // import Local from "./Components/Local";
 import Notes from "./Components/Notes";
 class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         <Notes />
       </div>
     );
